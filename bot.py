beed2041d167f6edc2bf3c13a831c112d6ef0ca6
bezrezen <bezrezen666@inbox.ru>
2022-05-16 13:26:59 +0300

    добавлен лог

--- bot.py
+++ bot.py
@@ -3,18 +3,16 @@ import botconfig
 import logging
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
-
 def main():
     bot = TB.TeleBot(botconfig.token)
     logging.info("Бот стартовал")
-    #with open(u'bot.log.','a') as f:
-        #f.write ('Бот стартовал')
-        #f.close()
+
 #приветствуем пользователя
     @bot.message_handler(commands=['start'])
     def hello_message(message):
         bot.send_message(message.chat.id,f"Привет! \n Ты выбрал /start")
         bot.send_message(message.chat.id,f"Это простой эхобот \n Отправь любое сообщение и получишь его в ответ")
+
 #эхуем последнее сообщение
     @bot.message_handler(content_types=['text'])
     def echo(message):
