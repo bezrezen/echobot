2a0fa388e207848babfe300b14d991150b57b13b
bezrezen <bezrezen666@inbox.ru>
2022-05-16 14:53:31 +0300

    исправлено

--- bot.py
+++ bot.py
@@ -5,13 +5,13 @@ logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 bot = TB.TeleBot(botconfig.token)
 
-
+#приветствуем пользователя
 @bot.message_handler(commands=['start'])
 def hello_message(message):
     bot.send_message(message.chat.id,f"Привет! \n Ты выбрал /start")
     bot.send_message(message.chat.id,f"Это простой эхобот \n Отправь любое сообщение и получишь его в ответ")
 
-
+#эхуем последнее сообщение
 @bot.message_handler(content_types=['text'])
 def echo(message):
     bot.send_message(message.chat.id,message.text)
@@ -23,14 +23,6 @@ def main():
     logging.info("Бот стартовал")
     hello_message()
     echo()
-
-#приветствуем пользователя
-
-
-#эхуем последнее сообщение
-    
-
-    
 
 if __name__ == "__main__":
     main()
