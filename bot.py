5ebd266509128cb0d951279b4293fa63387aa2e8
bezrezen <bezrezen666@inbox.ru>
2022-05-16 14:52:56 +0300

    исправлено

--- bot.py
+++ bot.py
@@ -3,22 +3,34 @@ import botconfig
 import logging
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
+bot = TB.TeleBot(botconfig.token)
+
+
+@bot.message_handler(commands=['start'])
+def hello_message(message):
+    bot.send_message(message.chat.id,f"Привет! \n Ты выбрал /start")
+    bot.send_message(message.chat.id,f"Это простой эхобот \n Отправь любое сообщение и получишь его в ответ")
+
+
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id,message.text)
+
+bot.infinity_polling()
+
 def main():
-    bot = TB.TeleBot(botconfig.token)
+    
     logging.info("Бот стартовал")
+    hello_message()
+    echo()
 
 #приветствуем пользователя
-    @bot.message_handler(commands=['start'])
-    def hello_message(message):
-        bot.send_message(message.chat.id,f"Привет! \n Ты выбрал /start")
-        bot.send_message(message.chat.id,f"Это простой эхобот \n Отправь любое сообщение и получишь его в ответ")
+
 
 #эхуем последнее сообщение
-    @bot.message_handler(content_types=['text'])
-    def echo(message):
-        bot.send_message(message.chat.id,message.text)
+    
 
-    bot.infinity_polling()
+    
 
 if __name__ == "__main__":
     main()
